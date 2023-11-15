b1afa65433faeeb5f6deab3dc0d5c7d54eea93b9
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2023-11-15 22:26:28 +0800

    fixed axios issues

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { PaymongoService } from './paymongo/paymongo.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, private readonly paymongo:PaymongoService ) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getCheckout() {
+    return await this.paymongo.createCheckOut([]);
   }
 }
